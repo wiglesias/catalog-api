f86c89b9388bb16d6b36a994257240ef56c2939d
Wils Iglesias <wiglesias83@gmail.com>
2026-03-10 14:12:30 +0100

    Create cruds with EasyAdmin

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
