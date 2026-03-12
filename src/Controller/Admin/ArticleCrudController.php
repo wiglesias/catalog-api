2e4c1b63554be8a4ccca80b5192f7f246258def5
Wils Iglesias <wiglesias83@gmail.com>
2026-03-12 16:58:13 +0100

    Add new fileds and improve ArticleType and UnitType enum display in CRUD

--- src/Controller/Admin/ArticleCrudController.php
+++ src/Controller/Admin/ArticleCrudController.php
@@ -3,10 +3,17 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Article;
+use App\Enum\ArticleType;
+use App\Enum\UnitType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -19,15 +26,47 @@ class ArticleCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        yield TextField::new('code', 'Article Code');
-        yield TextField::new('name');
-        yield ChoiceField::new('type')
-            ->setChoices([
-                'Raw Material' => 'RM',
-                'Packaging' => 'PK',
-                'Finished Good' => 'FG'
-            ]);
-        yield TextField::new('unit');
-        yield BooleanField::new('active');
+        yield FormField::addColumn(6, 'General');
+        yield TextField::new('code', 'Código');
+        yield TextField::new('name', 'Nombre');
+        yield ChoiceField::new('unit', 'Unidad')
+            ->setChoices(UnitType::cases())
+            ->setFormTypeOption('choice_label', fn(UnitType $unit) => $unit->label())
+            ->renderAsBadges()
+            ->formatValue(fn (?UnitType $unit) => $unit?->label());
+        yield ChoiceField::new('type', 'Tipo de artículo')
+            ->setChoices(ArticleType::cases())
+            ->setFormTypeOption('choice_label', fn(ArticleType $type) => $type->label())
+            ->renderAsBadges([
+                'raw_material' => 'warning',
+                'packaging' => 'info',
+                'component' => 'secondary',
+                'label' => 'primary',
+                'shipping_material' => 'dark',
+                'finished_product' => 'success',
+            ])
+            ->formatValue(fn (?ArticleType $type) => $type?->label());
+
+        yield TextField::new('barcode','Código de barras')->hideOnIndex();
+        yield BooleanField::new('active', 'Activo');
+        yield FormField::addColumn(6, 'Stock');
+        yield NumberField::new('netWeight','Peso neto')
+            ->setNumDecimals(4)
+            ->hideOnIndex();
+        yield NumberField::new('grossWeight','Peso bruto')
+            ->setNumDecimals(4)
+            ->hideOnIndex();
+        yield NumberField::new('length','Largo')
+            ->hideOnIndex();
+        yield NumberField::new('width','Ancho')
+            ->hideOnIndex();
+        yield NumberField::new('height','Alto')
+            ->hideOnIndex();
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return parent::configureActions($actions)
+            ->add(Crud::PAGE_DETAIL, Action::DETAIL);
     }
 }
